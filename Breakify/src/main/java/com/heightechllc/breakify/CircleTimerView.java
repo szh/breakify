4643c19ecd898f04fc9ae280df7d15ad637c0177
Shlomo Zalman Heigh <shlomozalmanheigh@gmail.com>
2014-07-09 19:46:48 -0400

    Add working animated timer
    Plus refactored, added logo, and a bunch of other things

---
+++ Breakify/src/main/java/com/heightechllc/breakify/CircleTimerView.java
@@ -0,0 +1,252 @@
+package com.heightechllc.breakify;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Round timer view.
+ * Based on CircleTimerView from the Android clock app (deskclock)
+ * Created by szh on 6/30/14.
+ */
+public class CircleTimerView extends View implements View.OnTouchListener {
+
+    private int redColor;
+    private int borderColor;
+    private int borderColorPressed;
+    private int backgroundColor;
+    private long totalIntervalTime = 0;
+    private long intervalStartTime = -1;
+    private long markerTime = -1;
+    private long currentIntervalTime = 0;
+    private long accumulatedTime = 0;
+    private boolean paused = false;
+    private boolean animate = false;
+    private static float strokeSize = 4;
+    private static float dotRadius = 6;
+    private static float markerStrokeSize = 2;
+    private final Paint borderPaint = new Paint();
+    private final Paint backgroundPaint = new Paint();
+    private final Paint redDotPaint = new Paint();
+    private final RectF arcRect = new RectF();
+    private float radiusOffset;   // amount to remove from radius to account for markers on circle
+    private float screenDensity;
+
+    /**
+     * Whether the view is currently pressed
+     */
+    private boolean pressed;
+
+    /**
+     * The listener to notify when the user clicks the View
+     */
+    private OnClickListener clickListener;
+
+    @SuppressWarnings("unused")
+    public CircleTimerView(Context context) {
+        this(context, null);
+    }
+
+    public CircleTimerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public void setIntervalTime(long t) {
+        totalIntervalTime = t;
+        postInvalidate();
+    }
+
+    public long getIntervalTime() {
+        return totalIntervalTime;
+    }
+
+    public void setMarkerTime(long t) {
+        markerTime = t;
+        postInvalidate();
+    }
+
+    public void reset() {
+        intervalStartTime = -1;
+        markerTime = -1;
+        postInvalidate();
+    }
+
+    public void startIntervalAnimation() {
+        intervalStartTime = SystemClock.elapsedRealtime();
+        animate = true;
+        invalidate();
+        paused = false;
+    }
+
+    public void stopIntervalAnimation() {
+        animate = false;
+        intervalStartTime = -1;
+        accumulatedTime = 0;
+    }
+
+    public boolean isAnimating() {
+        return (intervalStartTime != -1);
+    }
+
+    public void pauseIntervalAnimation() {
+        animate = false;
+        accumulatedTime += SystemClock.elapsedRealtime() - intervalStartTime;
+        paused = true;
+    }
+
+    public void abortIntervalAnimation() {
+        animate = false;
+    }
+
+    public void setPassedTime(long time, boolean drawRed) {
+        // The onDraw() method checks if intervalStartTime has been set before drawing any red.
+        // Without drawRed, intervalStartTime should not be set here at all, and would remain at -1
+        // when the state is reconfigured after exiting and re-entering the application.
+        // If the timer is currently running, this drawRed will not be set, and will have no effect
+        // because intervalStartTime will be set when the thread next runs.
+        // When the timer is not running, intervalStartTime will not be set upon loading the state,
+        // and no red will be drawn, so drawRed is used to force onDraw() to draw the red portion,
+        // despite the timer not running.
+        currentIntervalTime = accumulatedTime = time;
+        if (drawRed) {
+            intervalStartTime = SystemClock.elapsedRealtime();
+        }
+        postInvalidate();
+    }
+
+    public long getPassedTime() {
+        // Total time - remaining time
+        return totalIntervalTime - currentIntervalTime;
+    }
+
+
+    private void init(Context c) {
+
+        Resources resources = c.getResources();
+        strokeSize = resources.getDimension(R.dimen.circletimer_stroke_width);
+        dotRadius = resources.getDimension(R.dimen.circletimer_dot_size) / 2f;
+        markerStrokeSize = resources.getDimension(R.dimen.circletimer_marker_size);
+        radiusOffset = dotRadius;
+        borderPaint.setAntiAlias(true);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderColor = resources.getColor(R.color.timer_border);
+        borderColorPressed = resources.getColor(R.color.timer_border_pressed);
+        redColor = Color.RED;
+        backgroundColor = resources.getColor(R.color.clock_color);
+        screenDensity = resources.getDisplayMetrics().density;
+        redDotPaint.setAntiAlias(true);
+        redDotPaint.setStyle(Paint.Style.FILL);
+        redDotPaint.setColor(redColor);
+        backgroundPaint.setAntiAlias(true);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+        backgroundPaint.setColor(backgroundColor);
+
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        // Store the current value of `pressed`
+        boolean pressedValue = pressed;
+
+        // Check whether the user is pressing on the View
+        if (motionEvent.getActionMasked() == MotionEvent.ACTION_DOWN) pressed = true;
+        else if (motionEvent.getActionMasked() == MotionEvent.ACTION_UP) {
+            pressed = false;
+            // Notify the clickListener that the View has been clicked
+            if (clickListener != null) clickListener.onClick(this);
+        } else return false; // Ignore other event actions
+
+        // Redraw if the value of `pressed` has changed
+        if (pressedValue != pressed) invalidate();
+
+        return true;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        int xCenter = getWidth() / 2 + 1;
+        int yCenter = getHeight() / 2;
+
+        borderPaint.setStrokeWidth(strokeSize);
+        float radius = Math.min(xCenter, yCenter) - radiusOffset;
+
+        // Draw the background
+        canvas.drawCircle(xCenter, yCenter, radius, backgroundPaint);
+
+        if (intervalStartTime == -1) {
+            // just draw a complete white circle, no red arc needed
+            borderPaint.setColor(pressed ? borderColorPressed : borderColor);
+            canvas.drawCircle(xCenter, yCenter, radius, borderPaint);
+            drawRedDot(canvas, 0f, xCenter, yCenter, radius);
+        } else {
+            if (animate) {
+                currentIntervalTime = SystemClock.elapsedRealtime() - intervalStartTime + accumulatedTime;
+            }
+            //draw a combination of red and white arcs to create a circle
+            arcRect.top = yCenter - radius;
+            arcRect.bottom = yCenter + radius;
+            arcRect.left = xCenter - radius;
+            arcRect.right = xCenter + radius;
+            float redPercent = (float) currentIntervalTime / (float) totalIntervalTime;
+            // prevent timer from doing more than one full circle
+            redPercent = (redPercent > 1) ? 1 : redPercent;
+
+            float whitePercent = 1 - (redPercent > 1 ? 1 : redPercent);
+            // draw red arc here
+            borderPaint.setColor(redColor);
+            canvas.drawArc(arcRect, 270, -redPercent * 360, false, borderPaint);
+
+            // draw white arc here
+            borderPaint.setStrokeWidth(strokeSize);
+            borderPaint.setColor(pressed ? borderColorPressed : borderColor);
+            canvas.drawArc(arcRect, 270, +whitePercent * 360, false, borderPaint);
+
+            if (markerTime != -1 && radius > 0 && totalIntervalTime != 0) {
+                borderPaint.setStrokeWidth(markerStrokeSize);
+                float angle = (float) (markerTime % totalIntervalTime) / (float) totalIntervalTime * 360;
+                // draw 2dips thick marker
+                // the formula to draw the marker 1 unit thick is:
+                // 180 / (radius * Math.PI)
+                // after that we have to scale it by the screen density
+                canvas.drawArc(arcRect, 270 + angle, screenDensity *
+                        (float) (360 / (radius * Math.PI)), false, borderPaint);
+            }
+            drawRedDot(canvas, redPercent, xCenter, yCenter, radius);
+        }
+        if (animate) {
+            invalidate();
+        }
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        clickListener = l;
+    }
+
+    @Override
+    public boolean hasOnClickListeners() {
+        return clickListener != null;
+    }
+
+    protected void drawRedDot(
+            Canvas canvas, float degrees, int xCenter, int yCenter, float radius) {
+        borderPaint.setColor(redColor);
+        float dotPercent;
+        dotPercent = 270 - degrees * 360;
+
+        final double dotRadians = Math.toRadians(dotPercent);
+        canvas.drawCircle(xCenter + (float) (radius * Math.cos(dotRadians)),
+                yCenter + (float) (radius * Math.sin(dotRadians)), dotRadius, redDotPaint);
+    }
+
+}
--- Breakify/src/main/java/com/heightechllc/breakify/MainActivity.java
+++ Breakify/src/main/java/com/heightechllc/breakify/MainActivity.java
@@ -7,69 +7,64 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends Activity implements View.OnClickListener
-{
+public class MainActivity extends Activity implements View.OnClickListener {
     // Timer states
-    public static String RUNNING = "RUNNING";
-    public static String PAUSED = "PAUSED";
-    public static String STOPPED = "STOPPED";
+    public static int RUNNING = 1;
+    public static int PAUSED = 2;
+    public static int STOPPED = 0;
 
     // Work states
-    public static String WORK = "WORK";
-    public static String BREAK = "BREAK";
+    public static int WORK = 1;
+    public static int BREAK = 2;
 
     private String tag = "MainActivity";
 
     private CountDownTimer _countDownTimer;
-    private long millisRemaining;
-    private String timerState = STOPPED;
-    private String workState = WORK;
+    private int timerState = STOPPED;
+    private int workState = WORK;
+
+    // UI Components
+    CircleTimerView circleTimer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //
         // Set up components
         //
-        FrameLayout clockFrame = (FrameLayout) findViewById(R.id.clockFrame);
-        clockFrame.setOnClickListener(this);
+        circleTimer = (CircleTimerView) findViewById(R.id.circle_timer);
+        circleTimer.setOnClickListener(this);
 
-        NumberPicker workNumPicker = (NumberPicker) findViewById(R.id.workNumPicker);
+        NumberPicker workNumPicker = (NumberPicker) findViewById(R.id.work_num_picker);
         workNumPicker.setMinValue(1);
         workNumPicker.setMaxValue(300);
         workNumPicker.setValue(90);
 
-        NumberPicker breakNumPicker = (NumberPicker) findViewById(R.id.breakNumPicker);
+        NumberPicker breakNumPicker = (NumberPicker) findViewById(R.id.break_num_picker);
         breakNumPicker.setMinValue(1);
         breakNumPicker.setMaxValue(300);
         breakNumPicker.setValue(10);
 
-        Button resetBtn = (Button) findViewById(R.id.resetBtn);
+        Button resetBtn = (Button) findViewById(R.id.reset_btn);
         resetBtn.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view)
-    {
-        switch (view.getId())
-        {
-            case R.id.clockFrame:
-                // Either start or pause the timer
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.circle_timer:
                 if (timerState == RUNNING) pauseTimer();
                 else startTimer();
 
                 break;
-
-            case R.id.resetBtn:
+            case R.id.reset_btn:
                 resetTimer();
 
                 break;
@@ -78,73 +73,82 @@ public class MainActivity extends Activity implements View.OnClickListener
 
     /**
      * Starts the work or break timer
+     *
      * @param duration The number of milliseconds to run the timer for
      */
-    private void startTimer(long duration)
-    {
+    private void startTimer(long duration) {
         // First hide the number pickers and show the "Reset" btn (TODO: Animate)
-        findViewById(R.id.timerSettings).setVisibility(View.GONE);
-        findViewById(R.id.resetBtn).setVisibility(View.VISIBLE);
+        findViewById(R.id.timer_settings_layout).setVisibility(View.GONE);
+        findViewById(R.id.reset_btn).setVisibility(View.VISIBLE);
 
         // Update timer display
         updateTimeLbl(duration);
 
-        // Create the count down timer
-        _countDownTimer = new CountDownTimer(duration, 1000)
-        {
+        /*
+         *  Create the count down timer, which functions are:
+         *  1) To notify when the time is up
+         *  2) To notify every second to update the time display label
+         *
+         *  The function of keeping the exact time that has elapsed is taken care of by the
+         *  CircleTimerView, since it needs to refresh constantly anyway so it can animate, and it
+         *  would be silly to also require the CountDownTimer to tick often to record it as well.
+         */
+        _countDownTimer = new CountDownTimer(duration, 1000) {
             @Override
-            public void onTick(long millisUntilFinished)
-            {
+            public void onTick(long millisUntilFinished) {
                 // Update the UI
                 updateTimeLbl(millisUntilFinished);
-
-                // Record the time remaining
-                millisRemaining = millisUntilFinished;
             }
 
             @Override
-            public void onFinish()
-            {
+            public void onFinish() {
                 timerState = STOPPED;
                 // Update the time display
                 updateTimeLbl(0);
                 // Hide the start / pause btn (TODO: Animate)
-                findViewById(R.id.startStopLbl).setVisibility(View.INVISIBLE);
+                findViewById(R.id.start_stop_lbl).setVisibility(View.INVISIBLE);
 
                 // Prompt to start break / resume work with dialog
                 promptToSwitchWorkStates();
             }
         };
-        _countDownTimer.start();
-
-        timerState = RUNNING;
 
         // Update the start / stop label
-        TextView startStopLbl = (TextView) findViewById(R.id.startStopLbl);
+        TextView startStopLbl = (TextView) findViewById(R.id.start_stop_lbl);
         startStopLbl.setVisibility(View.VISIBLE);
         startStopLbl.setText(R.string.stop);
+
+        if (circleTimer.getIntervalTime() > 0 && timerState == PAUSED) {
+            // We're resuming from a paused state, so calculate how much time is remaining, based
+            //  on the total time set in the circleTimer (the intervalTime)
+            circleTimer.setPassedTime(circleTimer.getIntervalTime() - duration, false);
+        } else {
+            circleTimer.setIntervalTime(duration);
+            circleTimer.setPassedTime(0, false);
+        }
+
+        circleTimer.startIntervalAnimation();
+
+        _countDownTimer.start();
+        timerState = RUNNING;
     }
 
     /**
      * Starts the work or break timer
      * Calculates the duration automatically, and then calls startTimer(duration)
      */
-    private void startTimer()
-    {
+    private void startTimer() {
         long duration;
-        if (timerState == PAUSED)
-        {
+        if (timerState == PAUSED) {
             // Timer already started before, so just get the remaining time
-            duration = millisRemaining;
-        }
-        else
-        {
+            duration = circleTimer.getPassedTime();
+        } else {
             // Get chosen duration in minutes
             NumberPicker numberPicker;
             if (workState == WORK)
-                numberPicker = (NumberPicker) findViewById(R.id.workNumPicker);
+                numberPicker = (NumberPicker) findViewById(R.id.work_num_picker);
             else
-                numberPicker = (NumberPicker) findViewById(R.id.breakNumPicker);
+                numberPicker = (NumberPicker) findViewById(R.id.break_num_picker);
 
             duration = numberPicker.getValue() * 60000; // Multiply into milliseconds
         }
@@ -155,56 +159,59 @@ public class MainActivity extends Activity implements View.OnClickListener
     /**
      * Pauses the break timer
      */
-    private void pauseTimer()
-    {
-        timerState = PAUSED;
+    private void pauseTimer() {
+        // TODO: Blink time display while paused
+
         _countDownTimer.cancel();
+        timerState = PAUSED;
 
         // Update the start / stop label
-        TextView startStopLbl = (TextView) findViewById(R.id.startStopLbl);
+        TextView startStopLbl = (TextView) findViewById(R.id.start_stop_lbl);
         startStopLbl.setVisibility(View.VISIBLE);
         startStopLbl.setText(R.string.resume);
+
+        circleTimer.pauseIntervalAnimation();
     }
 
-    private void resetTimer()
-    {
+    private void resetTimer() {
         _countDownTimer.cancel();
         timerState = STOPPED;
 
         // Reset the UI (TODO: Animate)
-        TextView timeLbl = (TextView) findViewById(R.id.timeLbl);
+        TextView timeLbl = (TextView) findViewById(R.id.time_lbl);
         timeLbl.setText("");
 
-        findViewById(R.id.timerSettings).setVisibility(View.VISIBLE);
-        findViewById(R.id.resetBtn).setVisibility(View.GONE);
+        findViewById(R.id.timer_settings_layout).setVisibility(View.VISIBLE);
+        findViewById(R.id.reset_btn).setVisibility(View.GONE);
 
         // Back to initial state
         workState = WORK;
-        TextView stateLbl = (TextView) findViewById(R.id.stateLbl);
+        TextView stateLbl = (TextView) findViewById(R.id.state_lbl);
         stateLbl.setText(R.string.state_working);
 
         // Update the start / stop label
-        TextView startStopLbl = (TextView) findViewById(R.id.startStopLbl);
+        TextView startStopLbl = (TextView) findViewById(R.id.start_stop_lbl);
         startStopLbl.setVisibility(View.VISIBLE);
         startStopLbl.setText(R.string.start);
+
+        circleTimer.stopIntervalAnimation();
+        circleTimer.invalidate();
     }
 
 
-    private void updateTimeLbl(long millis)
-    {
+    private void updateTimeLbl(long millis) {
         // Get formatted time string
         String timeStr = formatTime(millis);
 
         // Update the clock
-        TextView timeLbl = (TextView) findViewById(R.id.timeLbl);
+        TextView timeLbl = (TextView) findViewById(R.id.time_lbl);
         timeLbl.setText(timeStr);
     }
 
     /**
      * Prompts the user to switch to the opposite work state (work or break)
      */
-    private void promptToSwitchWorkStates()
-    {
+    private void promptToSwitchWorkStates() {
         // Create the dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(false);
@@ -214,28 +221,22 @@ public class MainActivity extends Activity implements View.OnClickListener
         else
             builder.setMessage(R.string.start_work_prompt);
 
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
-        {
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 switchWorkStates();
             }
         });
-        builder.setNeutralButton(R.string.snooze, new DialogInterface.OnClickListener()
-        {
+        builder.setNeutralButton(R.string.snooze, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 // Snooze the timer for 3 minutes (TODO: Make this a setting)
                 startTimer(180000);
             }
         });
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener()
-        {
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 // User chose to cancel (TODO: Ask user to confirm?)
                 resetTimer();
             }
@@ -247,17 +248,13 @@ public class MainActivity extends Activity implements View.OnClickListener
     /**
      * Switches the workState to the opposite of the current and then starts the timer
      */
-    private void switchWorkStates()
-    {
+    private void switchWorkStates() {
         // Set the new state
-        TextView stateLbl = (TextView) findViewById(R.id.stateLbl);
-        if (workState == WORK)
-        {
+        TextView stateLbl = (TextView) findViewById(R.id.state_lbl);
+        if (workState == WORK) {
             workState = BREAK;
             stateLbl.setText(R.string.state_breaking);
-        }
-        else
-        {
+        } else {
             workState = WORK;
             stateLbl.setText(R.string.state_working);
         }
@@ -272,11 +269,11 @@ public class MainActivity extends Activity implements View.OnClickListener
 
     /**
      * Creates a formatted string representing a time value, in the format Min:Sec, e.g. 06:13
+     *
      * @param millis The number of milliseconds in the time
      * @return The formatted string
      */
-    private String formatTime(long millis)
-    {
+    private String formatTime(long millis) {
         // The '02' makes sure it's 2 digits
         return String.format("%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(millis),
