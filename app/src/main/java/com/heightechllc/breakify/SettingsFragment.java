6cab085a5296419334cc2712df545c0a1bdf91fc
Shlomo Zalman Heigh <shlomozalmanheigh@gmail.com>
2014-07-13 17:50:12 -0400

    Improve preferences UI
    Show current setting as “summary”, in compliance with the Android
    Design Guidelines (
    http://developer.android.com/design/patterns/settings.html#writing )

--- app/src/main/java/com/heightechllc/breakify/SettingsFragment.java
+++ app/src/main/java/com/heightechllc/breakify/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.heightechllc.breakify;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 
@@ -11,7 +13,7 @@ import android.preference.PreferenceFragment;
  * create an instance of this fragment.
  *
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     // Settings keys
     public static final String KEY_WORK_DURATION = "pref_key_work_duration";
@@ -32,7 +34,52 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        // Update the summaries for all of the preferences
+        String[] keys = {KEY_WORK_DURATION, KEY_BREAK_DURATION, KEY_SNOOZE_DURATION};
+        for (String key : keys)
+            updatePrefSummary(key);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        getPreferenceScreen().getSharedPreferences().
+                registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences().
+                unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // Check which preference changed
+        if (key.equals(KEY_WORK_DURATION) ||
+            key.equals(KEY_BREAK_DURATION) ||
+            key.equals(KEY_SNOOZE_DURATION))
+        {
+            // Refresh the preference that changed
+            updatePrefSummary(key);
+        }
+    }
+
+    /**
+     * Updates the summary of a preference with the current value, as recommended in the Android
+     *  Design Guidelines, at http://developer.android.com/design/patterns/settings.html#writing
+     * @param key The key of the preference to update
+     */
+    private void updatePrefSummary(String key) {
+        // Get the preference's value, and add " minutes" to make it human-readable
+        String summary = getPreferenceScreen().getSharedPreferences().getInt(key, 1)
+                                    + " " + getString(R.string.minutes);
+        // Set the preference's summary
+        Preference pref = findPreference(key);
+        pref.setSummary(summary);
+    }
 }
