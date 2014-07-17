78229150c384747b3ed6da0cf4e6350ba7d9d87c
Shlomo Zalman Heigh <shlomozalmanheigh@gmail.com>
2014-07-17 17:40:50 -0400

    Honor ringtone and vibration settings
    Also add default value for ringtone, and make the vibration pattern a
    local variable

--- app/src/main/java/com/heightechllc/breakify/AlarmRinger.java
+++ app/src/main/java/com/heightechllc/breakify/AlarmRinger.java
@@ -1,11 +1,13 @@
 package com.heightechllc.breakify;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.io.IOException;
@@ -16,9 +18,10 @@ import java.io.IOException;
  *  which is licensed under the Apache License, Version 2.0.
  */
 public class AlarmRinger {
-    private static final long[] vibratePattern = new long[] {500, 500};
+    private static String tag = "AlarmRinger";
 
     private static final float IN_CALL_VOLUME = 0.125f;
+    private static final int STREAM_TYPE = AudioManager.STREAM_ALARM;
 
     private static boolean started;
     private static MediaPlayer mediaPlayer;
@@ -40,33 +43,57 @@ public class AlarmRinger {
         // Make sure we are stopped before starting
         stop(context);
 
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mediaPlayer, int what, int extra) {
-                Log.e("AlarmRinger", "Error while playing alarm. Stopping AlarmRinger");
-                AlarmRinger.stop(context);
-                return true;
+        // Check which ringtone to use
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        String alarmUriStr = sharedPrefs.getString(SettingsFragment.KEY_RINGTONE, "");
+
+        // The URI Will be empty if set to "None". If it is, don't bother setting up a MediaPlayer
+        if (!alarmUriStr.isEmpty()) {
+            // Set up the media player
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                // Handles asynchronous errors. Synchronous exceptions are handled by try-catch
+                @Override
+                public boolean onError(MediaPlayer mediaPlayer, int what, int extra) {
+                    Log.e(tag, "Error while playing alarm");
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                    return true;
+                }
+            });
+
+            // If user is in a call, use a lower volume so we don't disrupt the call.
+            if (inTelephoneCall)
+                mediaPlayer.setVolume(IN_CALL_VOLUME, IN_CALL_VOLUME);
+
+            try {
+                Uri alarmUri = Uri.parse(alarmUriStr);
+                mediaPlayer.setDataSource(context, alarmUri);
+                startAlarm(context, mediaPlayer);
+            } catch (IOException e) {
+                try {
+                    Log.e(tag, "Failed to play selected ringtone. Trying to use default ringtone");
+                    e.printStackTrace();
+                    mediaPlayer.reset(); // Reset error state
+                    Uri defaultAlarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                    mediaPlayer.setDataSource(context, defaultAlarmUri);
+                    startAlarm(context, mediaPlayer);
+                } catch (IOException e1) {
+                    cleanUpMediaPlayer(context);
+                    Log.e(tag, "Failed to play default ringtone:");
+                    e1.printStackTrace();
+                }
             }
-        });
-
-        // If user is in a call, use a lower volume so we don't disrupt the call.
-        if (inTelephoneCall)
-            mediaPlayer.setVolume(IN_CALL_VOLUME, IN_CALL_VOLUME);
-
-        //TODO: Check which ringtone to use
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM); // Default alarm ringtone
-        try {
-            mediaPlayer.setDataSource(context, alarmUri);
-            startAlarm(context, mediaPlayer);
-        } catch (IOException e) {
-            cleanUpMediaPlayer(context);
-            Log.e("AlarmRinger", "Failed to play ringtone:");
-            e.printStackTrace();
         }
 
-        //TODO: Check if vibration is enabled
-        getVibrator(context).vibrate(vibratePattern, 0);
+        // Check if vibration is enabled
+        boolean vibrate = sharedPrefs.getBoolean(SettingsFragment.KEY_VIBRATE,
+                            context.getResources().getBoolean(R.bool.default_vibrate));
+
+        if (vibrate)
+            getVibrator(context).vibrate(new long[]{500, 500}, 0);
 
         started = true;
     }
@@ -78,13 +105,15 @@ public class AlarmRinger {
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
         // Don't play alarm if stream volume is 0 (typically because ringer mode is silent)
-        if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) == 0) return;
+        if (audioManager.getStreamVolume(STREAM_TYPE) == 0) {
+            cleanUpMediaPlayer(context);
+            return;
+        }
 
-        player.setAudioStreamType(AudioManager.STREAM_ALARM);
+        player.setAudioStreamType(STREAM_TYPE);
         player.setLooping(true);
         player.prepare();
-        audioManager.requestAudioFocus(null,
-                AudioManager.STREAM_ALARM, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+        audioManager.requestAudioFocus(null, STREAM_TYPE, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
         player.start();
     }
 
@@ -109,6 +138,7 @@ public class AlarmRinger {
         AudioManager audioManager = (AudioManager)
                 context.getSystemService(Context.AUDIO_SERVICE);
         audioManager.abandonAudioFocus(null);
+        mediaPlayer.reset(); // Fixes logcat warning "mediaplayer went away with unhandled events"
         mediaPlayer.release();
         mediaPlayer = null;
     }
